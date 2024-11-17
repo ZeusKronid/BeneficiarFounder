13ae7b1cd4e0cc5d01b5d1c7f870641bc73a44ae
ZeusKronid <150541236+ZeusKronid@users.noreply.github.com>
2024-11-17 04:37:13 +0300

    A bit of comments

--- BIV_script.py
+++ BIV_script.py
@@ -13,7 +13,6 @@ class BeneficiarFounder():
     If only graph is required call .build_full_graph()
     If need a drawing of graph call .draw_graph(indexes), reccomended no more than 10 nodes in indexes
     To have final pandas dataframe use .get_result_dataframe()
-    To load result in same folder with script call .download_result() the result will be saved in .tsv format
     
     '''
     
@@ -72,7 +71,9 @@ class BeneficiarFounder():
     
         Возвращает:
         tuple: два DataFrame — обновленные df_legal и df_natural.
+        
         """
+        
         # Объединяем оба DataFrame, чтобы учесть всех учредителей компании
         df_all = pd.concat([df_legal, df_natural], ignore_index=True)
         
@@ -103,7 +104,15 @@ class BeneficiarFounder():
         return df_legal_updated, df_natural_updated
 
     def _clean_data(self, ): 
+        """
+        Функция инициализации для подготовки данных к последующим вычислениям
+
+        1. Дропает na значения 
+        2. Делает id уникальными для legal и natural
+        3. Восстанавливает проценты используя метод _restore_share_percent
+        4. Округляет полученные значения share_percent до 4 знаков
         
+        """
         self.company = self.company.dropna()
         self.founder_legal = self.founder_legal.dropna(subset=['ogrn', 'inn'])
 
@@ -127,6 +136,15 @@ class BeneficiarFounder():
         return self.founder_legal_cleared, self.founder_natural_cleared
 
     def _init_owners_dict(self, ): 
+        '''
+        Функци инициализирует два словаря владений, которые в последствии будут 
+        использоваться при построении графов. 
+        Ключ - id компании
+        Значение - все овнеры
+
+        Для ускорения вычислений использованы DataFrame от библиотеки Polars.
+        
+        '''
                 
         founder_natural_cleared_pl = pl.from_pandas(self.founder_natural_cleared)
         founder_legal_cleared_pl = pl.from_pandas(self.founder_legal_cleared)
@@ -146,25 +164,15 @@ class BeneficiarFounder():
             .agg([pl.struct(['uid', 'share_percent']).alias('owners')])
             .to_dicts()
         }
-
-        # natural_owners_df = (
-        #     founder_natural_cleared_pl
-        #     .group_by('company_id')
-        #     .agg([pl.struct(['uid', 'share_percent']).alias('owners')])
-        # )
-        
-        # self.natural_owners_dict = dict(zip(natural_owners_df['company_id'], natural_owners_df['owners']))
-        
-        # legal_owners_df = (
-        #     founder_legal_cleared_pl
-        #     .group_by('company_id')
-        #     .agg([pl.struct(['uid', 'share_percent']).alias('owners')])
-        # )
-
-        # self.legal_owners_dict = dict(zip(legal_owners_df['company_id'], legal_owners_df['owners']))
         
 
     def _build_graph_for_node(self, company_id, company_df, founder_legal_df, founder_natural_df, G=None):
+        '''        
+        Функция циклично вычисляет граф-дерево владений для определенной компании. 
+        Была использована двусторонняя очередь в качестве структуры данных для значительного ускорения
+        вычислений. (Вычисление в обычной очереди реализованной через список - О(n), вычисления в deque - О(1))
+    
+        '''
         
         if G is None:
             G = nx.Graph()
@@ -174,7 +182,7 @@ class BeneficiarFounder():
         while queue:
             current_company = queue.popleft()
             
-            # Добавляем основную компанию в граф
+            # Добавляем компанию в граф
             G.add_node(current_company, type="company")
             
             # Извлекаем владельцев
@@ -196,8 +204,12 @@ class BeneficiarFounder():
     
 
     def build_full_graph(self, ): 
+        '''
+        Функция для построения полного графа. 
+        
+        '''
 
-        G = nx.Graph()  # Начальный пустой графDiGraph
+        G = nx.Graph() 
         for idx, row in self.company_cleared.iterrows():
             company_id = row['uid']
             G = self._build_graph_for_node(company_id, self.company_cleared, self.founder_legal_cleared, self.founder_natural_cleared, G)
@@ -207,9 +219,10 @@ class BeneficiarFounder():
         
     def draw_graph(self, draw_indexes): 
         """
-        Строит и рисует граф владения для всех компаний.
+        Строит и рисует граф владения для заданных индексов.
+        
         """
-        G = nx.Graph()  # Начальный пустой графDiGraph
+        G = nx.Graph()  
         
         # Строим граф для каждой компании из company_df
         for idx, row in self.company_cleared[draw_indexes[0] : draw_indexes[1]].iterrows():
@@ -247,9 +260,13 @@ class BeneficiarFounder():
         Для каждой начальной вершины находит конечные ноды (с типом 'natural') 
         и считает долю владения на основе произведения весов рёбер.
         Возвращает словарь формата:
+        
         {
             'ВЕРШИНА': {'НОДА1': доля, 'НОДА2': доля}
         }
+        In: G - граф, start_nodes - начальные вершины( python.tsv)
+        Out: dict results
+        
         """
         results = {}
     
@@ -266,7 +283,7 @@ class BeneficiarFounder():
             if G.nodes[node]['type'] == 'natural':
                 return {node: current_share}
     
-            # Рекурсивно обходим всех соседей
+            # обходим всех соседей
             shares = {}
             for neighbor in G.neighbors(node):
                 edge_weight = G[node][neighbor].get('weight', 1)
@@ -292,7 +309,9 @@ class BeneficiarFounder():
         """
         Фильтрует конечные узлы (natural) в словаре владений, оставляя только те,
         чья доля больше заданного порога (threshold).
+        
         """
+        
         filtered_shares = {}
         for node, shares in ownership_shares.items():
             # Фильтруем вложенные словари по значению (доля > threshold)
@@ -306,6 +325,10 @@ class BeneficiarFounder():
 
         
     def _find_beneficiars(self, G): 
+        '''
+        Вспомогательная функция, соединяющая _calculate_final_shares и _filter_ownership_shares в одну
+        
+        '''
         
         shares = self._calculate_final_shares(G, self.company_cleared['uid'].tolist())
         benef = self._filter_ownership_shares(shares, threshold=0.25)
@@ -316,7 +339,11 @@ class BeneficiarFounder():
         """
         Конвертирует словарь владений в Pandas DataFrame с колонками:
         'Вершина', 'Нода', 'Доля'.
+        In: словарь владений ownership_shares вида  {'ВЕРШИНА': {'НОДА1': доля, 'НОДА2': доля} }
+        Out: pandas DF с столбцами uid_cp, uid_nt, share_percentage
+        
         """
+        
         rows = []
         for node, shares in ownership_shares.items():
             for natural_node, share in shares.items():
@@ -331,22 +358,36 @@ class BeneficiarFounder():
 
         
     def get_result_dataframe(self, ): 
+        '''
+        Использует функцию build_full_graph для построения графа. 
+        Определяет бенефициаров в словаре владений
+        Путём добавления данных по индексам из изначальных таблиц строит результирующий датафрейм. 
+        
+        '''
+
+        # Получаем граф владения
         G = self.build_full_graph()
-        
+        # Определяем словарь владений, сортированный до бенефициаров и переводим его во фрейм 
         benef = self._find_beneficiars(G)
-        
         df = self._convert_to_dataframe(benef)
-        
+
+        #Мерджим индексы uid_cp с очищенным датафреймом для получения id
         self.company_cleared['uid_cp'] = self.company_cleared['uid']
         result = df.merge(self.company_cleared, how='left', on='uid_cp').drop(['uid_cp', 'uid'], axis=1)
-        
+        # по id мерджим фрейм с итоговым датасетом
         result = result.merge(self.company, how='left', on='id')
+
+        #Аналогично проделываем для founders
         self.founder_natural_cleared['uid_nt'] = self.founder_natural_cleared['uid']
         result = result.merge(self.founder_natural_cleared, how='left', on='uid_nt').drop(['company_id', 'share', 'share_percentage', 'uid_nt', 'uid',],axis=1)
         self.founder_natural['id_y'] = self.founder_natural['id']
         result = result.merge(self.founder_natural, how='left', on='id_y').drop(['id_y', 'share_percent_y', 'id', 'company_id', 'share'],axis=1)
+
+        # для правильного расположения столбцов
         result['share_percent'] = result['share_percent_x']
         result = result.drop(['share_percent_x'],axis=1)
+
+        #Определяем необходимые названия столбцов
         result = result.rename(columns={'id_x': 'company_id', 'inn_x': 'company_inn', 'inn_y': 'natural_inn'})
         
         return result
--- run.py
+++ run.py
@@ -3,17 +3,25 @@ import csv
 import polars as pl
 
 if __name__=='__main__':
+    '''
+    Функция-ранер, использующая класс BeneficiarFounder. 
+    Создана для запуска в докере, расположение файлов указано для вмонтированной папки
+    Правильный запуск: docker run --rm -v $/path/to/files:/app docker_image_name
+
+    Полученный с помощью BeneficiarFounder датафрейм переводится в polars для ускорения компоновки итоговой tsv таблицы. 
+    
+    '''
+    
     company_file = '/app/company.tsv'
     legal_file = '/app/founder_legal.tsv'
     natural_file = '/app/founder_natural.tsv'
     
     founder = BeneficiarFounder(company_file, legal_file, natural_file)
-
     frame = founder.get_result_dataframe()
 
+    #Значительно ускоряет дальнейшие вычисления
     frame = pl.from_pandas(frame)
-
-
+    
     # Открываем выходной файл для записи
     with open('/app/output_file.tsv', 'w') as f:
         header = "company_id\togrn/natural_inn\tcompany_inn/last_name\tfull_name/first_name\tsecond_name\tshare_percent\n"
@@ -42,24 +50,4 @@ if __name__=='__main__':
                 natural_inn, last_name, first_name, second_name, share_percent = row
                 line2 = f"\t{natural_inn}\t{last_name}\t{first_name}\t{second_name}\t{share_percent}\n"
                 f.write(line2)
-        
-    # # Открываем выходной файл для записи
-    # with open('output_file.tsv', 'w') as f:
-    #     header = "company_id\togrn/natural_inn\tcompany_inn/last_name\tfull_name/first_name\tsecond_name\tshare_percent\n"
-    #     f.write(header)
-        
-    #     # Группируем по полям компании
-    #     grouped = frame.groupby(['company_id', 'ogrn', 'company_inn', 'full_name'])
-    
-    #     # Проходим по каждой группе
-    #     for _, group in grouped:
-    #         # Формируем строку с данными компании
-    #         line1 = f"{group['company_id'].iloc[0]}\t{group['ogrn'].iloc[0]}\t{group['company_inn'].iloc[0]}\t{group['full_name'].iloc[0]}\t\t\n"
-            
-    #         # Записываем строку с данными компании
-    #         f.write(line1)
-            
-    #         # Проходим по строкам группы, начиная с второй (где есть natural_inn)
-    #         for _, row in group.iterrows():
-    #             line2 = f"\t{row['natural_inn']}\t{row['last_name']}\t{row['first_name']}\t{row['second_name']}\t{row['share_percent']}\n"
-    #             f.write(line2)
+ 
